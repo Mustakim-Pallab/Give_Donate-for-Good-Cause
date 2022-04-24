4eeb4544bf042cb9b918d22c0db01a069ffc31b8
Pallab <mustakimpallab2434@gmail.com>
2022-04-24 13:29:37 +0600

    maybe the last commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA2VqQ1zJuXyrFX5qloOwm7zwB8K7DujxQ",
+  authDomain: "give-3d6fc.firebaseapp.com",
+  projectId: "give-3d6fc",
+  storageBucket: "give-3d6fc.appspot.com",
+  messagingSenderId: "366183371730",
+  appId: "1:366183371730:web:18dd57b53e61a8e87dae0b"
+};
+
+  export default firebaseConfig;
